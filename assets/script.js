87b21a44cc65ec930c3bd114446eb73d3b0d00e7
Samy Belkacem <Samywinseverytime@hotmail.com>
2023-11-16 20:56:44 -0500

    initial commit

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,44 @@
-var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", writePassword);
-
-function writePassword() {
+document.getElementById("generate").addEventListener("click", function () {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
+  document.getElementById("password").value = password;
+});
 
 function generatePassword() {
-  // Prompt for password length
-  var length = parseInt(prompt("Enter the length of the password (between 8 and 128 characters):"));
+  var length = 8; 
 
-  // Validate the length
-  if (isNaN(length) || length < 8 || length > 128) {
-    alert("Please enter a valid password length between 8 and 128 characters.");
-    return "";
+  var includeLowercase = true;
+  var includeUppercase = true;
+  var includeNumeric = true;
+  var includeSpecial = true;
+
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_-+=";
+
+  var allChars = "";
+  if (includeLowercase) {
+    allChars += lowercaseChars;
+  }
+  if (includeUppercase) {
+    allChars += uppercaseChars;
+  }
+  if (includeNumeric) {
+    allChars += numericChars;
+  }
+  if (includeSpecial) {
+    allChars += specialChars;
   }
 
-  // Confirm character types
-  var includeLowercase = confirm("Include lowercase characters?");
-  var includeUppercase = confirm("Include uppercase characters?");
-  var includeNumeric = confirm("Include numeric characters?");
-  var includeSpecial = confirm("Include special characters?");
-
-  // Validate that at least one character type is selected
-  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+  if (allChars === "") {
     alert("Please select at least one character type.");
     return "";
   }
 
-  // Generate password based on selected criteria
-  var charset = "";
-  if (includeLowercase) charset += "abcdefghijklmnopqrstuvwxyz";
-  if (includeUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  if (includeNumeric) charset += "0123456789";
-  if (includeSpecial) charset += "!@#$%^&*()_+";
-
   var password = "";
   for (var i = 0; i < length; i++) {
-    var randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset[randomIndex];
+    var randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars.charAt(randomIndex);
   }
 
   return password;
